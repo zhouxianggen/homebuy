c1eae94bf14cc3d0add282f99cf84a7bb41121d0
zhouxianggen <zhouxianggen@gmail.com>
2016-01-20 23:10:25 +0800

    update

--- selller/app/src/main/java/com/hg/www/selller/EditCommodityActivity.java
+++ selller/app/src/main/java/com/hg/www/selller/EditCommodityActivity.java
@@ -1,18 +1,37 @@
 package com.hg.www.selller;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.CheckBox;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hg.www.selller.define.Commodity;
+import com.hg.www.selller.util.HorizontalListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EditCommodityActivity extends AppCompatActivity {
+    private static final String TAG = "EditCommodityActivity";
+    private static final int RESULT_CODE_ADD_IMAGE = 1;
     private Commodity mCommodity;
+    private HorizontalListView mListView;
+    private ImageListAdapter mAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +43,30 @@ public class EditCommodityActivity extends AppCompatActivity {
         mCommodity = ((MyApplication)getApplication()).getDataManager().GetCommodity(commodityId);
         initializeActionBar();
         initializeView();
+        mListView = (HorizontalListView) findViewById(R.id.image_list);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdapter == null) {
+            mAdapter = new ImageListAdapter();
+            mListView.setAdapter(mAdapter);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult with request code " + String.valueOf(requestCode));
+        Log.d(TAG, "onActivityResult with result code " + String.valueOf(resultCode));
+        if (requestCode == RESULT_CODE_ADD_IMAGE) {
+            if (resultCode == RESULT_OK) {
+                Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+                mAdapter.addImage(thumbnail);
+                mAdapter.notifyDataSetChanged();
+            }
+        }
     }
 
     private void initializeActionBar() {
@@ -34,9 +77,9 @@ public class EditCommodityActivity extends AppCompatActivity {
         View customView = actionBar.getCustomView();
         TextView textView = (TextView) customView.findViewById(R.id.title);
         if (mCommodity.id.isEmpty()) {
-            textView.setText(((MyApplication)getApplication()).getString(R.string.add_commodity));
+            textView.setText(getApplication().getString(R.string.add_commodity));
         } else {
-            textView.setText(((MyApplication)getApplication()).getString(R.string.edit_commodity));
+            textView.setText(getApplication().getString(R.string.edit_commodity));
         }
     }
 
@@ -48,6 +91,116 @@ public class EditCommodityActivity extends AppCompatActivity {
             findViewById(R.id.option_in_discount).setSelected(mCommodity.is_in_discount);
             findViewById(R.id.option_support_return).setSelected(mCommodity.is_support_return);
             ((EditText) findViewById(R.id.desc)).setText(mCommodity.desc);
+        }
+    }
+
+    class ImageListAdapter extends BaseAdapter {
+        private List<Bitmap> mImages = new ArrayList<>();
+
+        ImageListAdapter() {
+            Rect rect = new Rect(0, 0, 1, 1);
+            Bitmap image = Bitmap.createBitmap(rect.width(), rect.height(), Bitmap.Config.ARGB_8888);
+            mImages.add(image);
+        }
+
+        public void deleteItem(int position) {
+            if (position + 1 < mImages.size()) {
+                mImages.remove(position);
+            }
+        }
+
+        public void addImage(Bitmap bitmap) {
+            mImages.add(mImages.size()-1, bitmap);
+        }
+
+        @Override
+        public int getCount() {
+            return mImages.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mImages.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return mImages.get(position).hashCode();
+        }
+
+        @Override
+        public boolean areAllItemsEnabled() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled(int position) {
+            return true;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Log.d(TAG, "getView " + String.valueOf(position));
+            View view;
+            if (convertView != null) {
+                view = convertView;
+            } else if (position + 1 < mImages.size()) {
+                view = LayoutInflater.from(parent.getContext()).inflate(
+                        R.layout.image_list_item, null);
+                ImageView imageView = (ImageView) view.findViewById(R.id.image);
+                imageView.setImageBitmap(mImages.get(position));
+            } else {
+                view = LayoutInflater.from(parent.getContext()).inflate(
+                        R.layout.add_image_btn, null);
+            }
+            ListItemViewHolder holder = (ListItemViewHolder) view.getTag();
+            if (holder == null) {
+                holder = new ListItemViewHolder();
+                if (position + 1 < mImages.size()) {
+                    holder.deleteImageButtonOnClickListener = new DeleteImageButtonOnClickListener();
+                    holder.deleteImageButtonOnClickListener.position = position;
+                    holder.deleteImageButton = (Button) view.findViewById(R.id.btn_delete_image);
+                    holder.deleteImageButton.setOnClickListener(holder.deleteImageButtonOnClickListener);
+                } else {
+                    holder.addImageButtonOnClickListener = new AddImageButtonOnClickListener();
+                    holder.addImageButtonOnClickListener.position = position;
+                    holder.addImageButton = (Button) view.findViewById(R.id.btn_add_image);
+                    holder.addImageButton.setOnClickListener(holder.addImageButtonOnClickListener);
+                }
+                view.setTag(holder);
+            }
+            return view;
+        }
+    }
+
+    ;
+
+    static class ListItemViewHolder {
+        public Button deleteImageButton;
+        public Button addImageButton;
+        public DeleteImageButtonOnClickListener deleteImageButtonOnClickListener;
+        public AddImageButtonOnClickListener addImageButtonOnClickListener;
+    }
+
+    class DeleteImageButtonOnClickListener implements View.OnClickListener {
+        public int position;
+
+        @Override
+        public void onClick(View view) {
+            Log.d(TAG, "delete " + String.valueOf(position));
+            mAdapter.deleteItem(position);
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    class AddImageButtonOnClickListener implements View.OnClickListener {
+        public int position;
+
+        @Override
+        public void onClick(View view) {
+            Log.d(TAG, "add " + String.valueOf(position));
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, RESULT_CODE_ADD_IMAGE);
         }
     }
 }
---
+++ selller/app/src/main/java/com/hg/www/selller/MainActivity.java
@@ -0,0 +1,193 @@
+package com.hg.www.selller;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Handler;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener {
+
+    private static final long DRAWER_CLOSE_DELAY_MS = 250;
+    private static final String NAV_ITEM_ID = "navItemId";
+    private final Handler mDrawerActionHandler = new Handler();
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private int mNavItemId;
+    private TabsPagerAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // load saved navigation state if present
+        if (null == savedInstanceState) {
+            mNavItemId = R.id.drawer_item_1;
+        } else {
+            mNavItemId = savedInstanceState.getInt(NAV_ITEM_ID);
+        }
+
+        // listen for navigation events
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // select the correct nav menu item
+        navigationView.getMenu().findItem(mNavItemId).setChecked(true);
+
+        // set up the hamburger icon to open and close the drawer
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open,
+                R.string.close);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+        navigate(mNavItemId);
+
+        mAdapter = new TabsPagerAdapter(getSupportFragmentManager(), getApplicationContext());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(mAdapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void navigate(final int itemId) {
+        // perform the actual navigation logic, updating the main content fragment etc
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(final MenuItem menuItem) {
+        // update highlighted item in the navigation menu
+        menuItem.setChecked(true);
+        mNavItemId = menuItem.getItemId();
+
+        // allow some time after closing the drawer before performing real navigation
+        // so the user can see what is happening
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        mDrawerActionHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                navigate(menuItem.getItemId());
+            }
+        }, DRAWER_CLOSE_DELAY_MS);
+        return true;
+    }
+
+    @Override
+    public void onConfigurationChanged(final Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        if (item.getItemId() == android.support.v7.appcompat.R.id.home) {
+            return mDrawerToggle.onOptionsItemSelected(item);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(final Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(NAV_ITEM_ID, mNavItemId);
+    }
+
+    public static class TabsPagerAdapter extends FragmentPagerAdapter {
+        private Context context;
+
+        public TabsPagerAdapter(FragmentManager fm, Context context) {
+            super(fm);
+            this.context = context;
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            switch (i) {
+                case 0:
+                    return new CommodityFragment();
+                case 1:
+                    return new OrderFragment();
+                case 2:
+                    return new DealFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return context.getString(R.string.tab_commodity);
+                case 1:
+                    return context.getString(R.string.tab_order);
+                case 2:
+                    return context.getString(R.string.tab_deal);
+            }
+            return null;
+        }
+    }
+
+    public static class CommodityFragment extends  Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_commodity, container, false);
+            return rootView;
+        }
+    }
+
+    public static class OrderFragment extends  Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_order, container, false);
+            return rootView;
+        }
+    }
+
+    public static class DealFragment extends  Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_deal, container, false);
+            return rootView;
+        }
+    }
+}
